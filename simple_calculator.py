5c6ba792766036e71bf8e3302ae372e0922f232a
abuhurayra15 <arafatnice333@gmail.com>
2026-03-13 20:58:47 +0600

    add Simple Calculator Basic

---
+++ simple_calculator.py
@@ -0,0 +1,12 @@
+num1 = int(input("Pick first number: "))
+num2 = int(input("Pick second number:"))
+
+Addition = num1+num2
+Subtraction =num1-num2
+multiplication = num1*num2
+division = num1/num2
+
+print("addition is : ",Addition)
+print("Addition is : ",Subtraction)
+print("Multiplication is : ",multiplication)
+print("Division is : ",division )
