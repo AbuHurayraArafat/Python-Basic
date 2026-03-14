67b99bd8f71703e4944863c1f39d76f91ed3b678
abuhurayra15 <arafatnice333@gmail.com>
2026-03-14 11:28:08 +0600

    add basic calculator example by loop

---
+++ calculator_example.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number  : "))
+num2 = int(input("Enter second number : "))
+
+choice = input("Enter choice operation (+, -, *, /) : ")
+
+if choice == "+":
+    print("Result = ",num1+num2)
+if choice == "-":
+    print("Result = ",num1-num2)
+if choice == "*":    
+    print("Result = ",num1+num2)
+if choice == "/":    
+    print("Result = ",num1+num2)
+
